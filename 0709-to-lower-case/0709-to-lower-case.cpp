ab12c0298b279f741a23ca462a34596afbe4e6cc
Anmol Narang <76910737+ianmolcode@users.noreply.github.com>
2022-12-02 03:40:42 +0530

    Time: 0 ms (100.00%), Space: 6 MB (81.06%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        int n = s.length();
+        for(int i = 0;i<n;i++) if(s[i] == toupper(s[i])) s[i] = tolower(s[i]);
+        return s;
+    }
+};
