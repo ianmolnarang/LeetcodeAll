e7081b362486e9307ad421992eba4b664735ac9d
Anmol Narang <76910737+ianmolcode@users.noreply.github.com>
2023-02-14 18:27:41 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (78.08%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,27 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+    
+    
+   
+vector<int> ans;
+   void inorder(TreeNode* root)
+   {
+       if(root==NULL) return;
+       inorder(root->left);
+       ans.push_back(root->val);
+       inorder(root->right);
+   }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        TreeNode* curr = root;
-        while(curr){
-            if(curr->left){
-                TreeNode* temp = curr->left;
-                while(temp->right and temp->right != curr){
-                    temp = temp->right;
-                }
-                if(!temp->right){
-                    temp->right = curr;  
-                    curr = curr->left;
-                }else{
-                    ans.push_back(curr->val);
-                    curr = curr->right;
-                    temp->right = NULL;
-                }
-            }else{
-                ans.push_back(curr->val);
-                curr = curr->right;
-            }
-        }
+        inorder(root);
         return ans;
     }
 };
