b861d9e58ed9411461551e04d0896d519405c2fd
Anmol Narang <76910737+ianmolcode@users.noreply.github.com>
2023-06-08 01:09:47 +0000

    Time: 4 ms (83.28%), Space: 10.7 MB (70.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,35 +1,31 @@
 class Solution {
+
 public:
-    vector<vector<int>> ans ; 
-    
-    void helper(vector<int>& candidates, int target , int i , int currsum , vector<int> temp){
-      //bounding condition
-        if(currsum>target)
-           { 
-              return;
-        } 
-        //base case 
-        if(i==candidates.size()){
-            if(currsum ==  target) 
-               { ans.push_back(temp) ;
-               } 
-            return ; 
-        } 
-        //inclusion 
-        currsum +=  candidates[i] ; 
-        temp.push_back(candidates[i]);
-        helper(candidates, target, i, currsum , temp ) ;
-        //if condition is failed then we have to take out the last element and backtrack
-         currsum -=  candidates[i] ; 
-        temp.pop_back() ; 
-        
-        helper(candidates, target, i+1, currsum , temp ) ;
-        
+  void f(vector<int>&arr, int target , int ind , vector<vector<int>>&ans, vector<int>&ds)     {
+      //base condition 
+      if(ind == arr.size()){
+          if(target==0) 
+{              ans.push_back(ds) ;}
+          return ; 
+      }
+      
+      //recursion call 
+      //using pick and not pick method 
+      
+      //pick 
+      
+      if(arr[ind]<=target){
+          ds.push_back(arr[ind]) ;
+          f(arr , target- arr[ind] , ind  , ans , ds )  ;
+          ds.pop_back() ;       
+      }
+      f(arr , target, ind+1  , ans , ds )  ;
         
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> temp ; 
-        helper(candidates, target, 0 , 0 , temp) ; 
+        vector<vector<int>> ans ; 
+        vector<int> ds  ;
+        f(candidates , target , 0 ,  ans , ds)  ;
         return ans ; 
     }
 };
